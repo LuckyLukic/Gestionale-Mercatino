e3cee4e279bc48b4841b01125e0c5f112b6a8084
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-26 20:03:46 +0100

    database buttons

--- resources/views/livewire/data.blade.php
+++ resources/views/livewire/data.blade.php
@@ -2,7 +2,7 @@
 
     <h1 class="text-center mb-8">UTENTI</h1>
 
-    <div class="grid grid-cols-4 max-w-7xl mx-auto px-8 mb-2">
+    <div class="grid grid-cols-4 max-w-7xl mx-auto px-8 mb-5">
         <div> Nome e Cognome</div>
         <div> Città</div>
         <div> email</div>
@@ -17,7 +17,13 @@
 
 
             <div> {{$user->email}}</div>
-            <div> azione</div>
+            <div class="grid grid-cols-3 gap-x-2">
+
+                <a href="" class="w-full"><button type="button" class="w-full bg-lime-500 hover:bg-lime-700 hover:scale-105 text-zinc-200 rounded px-2 py-1 transition ease-in-out delay-50 duration-200">View</button> </a>
+                <a href="" class="w-full"><button type="button" class="w-full bg-sky-500 hover:bg-sky-700 hover:scale-105 text-zinc-200 rounded px-2 py-1 transition ease-in-out delay-50 duration-200">Update</button></a>
+                <button type="button" class="w-full bg-red-500 hover:bg-red-700 hover:scale-105 text-zinc-200 rounded px-2 py-1 transition ease-in-out delay-50 duration-200 ">Remove</button>
+
+            </div>
         </div>
     @endforeach
 </div>
